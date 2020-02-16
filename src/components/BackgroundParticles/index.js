9b9fbd57d147bffe51a9d3812df318afe41916ae
Juliano Sirtori <julianosirtori@gmail.com>
2020-02-16 18:27:20 -0300

    Initial Commit

---
+++ src/components/BackgroundParticles/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+export default function BackgroundParticles({ children }) {
+  return (
+    <>
+      <Particles
+        style={{
+          background: '#232741',
+          position: 'absolute',
+          zIndex: -1,
+        }}
+        height="100vh"
+        params={{
+          particles: {
+            number: {
+              value: 160,
+              density: {
+                enable: false,
+              },
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 1,
+              direction: 'top',
+              out_mode: 'out',
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'bubble',
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+      />
+      {children}
+    </>
+  );
+}
